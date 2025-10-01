57fa3dbe65f639d09a91a6369e35e0de33252bfe
Juan Pablo <juanpablo10056@gmail.com>
2025-09-30 22:53:22 -0500

    Se creo UnitSalesDomain

---
+++ src/main/java/co/edu/uco/aurora/business/domain/UnitSalesDomain.java
@@ -0,0 +1,34 @@
+package co.edu.uco.aurora.business.domain;
+
+import co.edu.uco.aurora.crosscuting.helper.TextHelper;
+import co.edu.uco.aurora.crosscuting.helper.UUIDHelper;
+
+import java.util.UUID;
+
+public final class UnitSalesDomain extends Domain{
+
+    private String name;
+
+    public UnitSalesDomain() {
+        super(UUIDHelper.getUUIDHelper().getDefault());
+        setName(TextHelper.getDefault());
+    }
+
+    public UnitSalesDomain(final UUID id) {
+        super(id);
+        setName(TextHelper.getDefault());
+    }
+
+    public UnitSalesDomain (final UUID id, final String name) {
+        super (id);
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = TextHelper.getDefaultWithTrim(name);
+    }
+}
