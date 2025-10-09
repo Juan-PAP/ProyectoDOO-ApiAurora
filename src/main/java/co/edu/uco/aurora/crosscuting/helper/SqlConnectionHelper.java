c123feee1399d1cbef191ebe55f962130cd620b7
Juan Pablo <juanpablo10056@gmail.com>
2025-10-09 16:21:34 -0500

    SqlConnectionHelper creado y mensajes Enum creados he implementados

---
+++ src/main/java/co/edu/uco/aurora/crosscuting/helper/SqlConnectionHelper.java
@@ -0,0 +1,96 @@
+package co.edu.uco.aurora.crosscuting.helper;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import co.edu.uco.aurora.crosscuting.exception.AuroraException;
+import co.edu.uco.aurora.crosscuting.messagescatalog.MessagesEnum;
+
+public class SqlConnectionHelper {
+
+    private SqlConnectionHelper() {
+    }
+
+    public static void ensureConnectionIsNotNull(final Connection connection) {
+        if (ObjectHelper.isNull(connection)) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_IS_EMPTY.getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_IS_EMPTY.getContent();
+            throw AuroraException.create(userMessage, technicalMessage);
+        }
+    }
+
+    public static void ensureConnectionIsOpen(final Connection connection) {
+
+        ensureConnectionIsNotNull(connection);
+
+        try {
+            if (connection.isClosed()) {
+                var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_IS_CLOSED.getContent();
+                var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_IS_CLOSED.getContent();
+                throw AuroraException.create(userMessage, technicalMessage);
+            }
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_CONNECTION_STATUS
+                    .getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_SQL_EXCEPTION_VALIDATING_CONNECTION_STATUS
+                    .getContent();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        } catch (final Exception exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_CONNECTION_STATUS
+                    .getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_CONNECTION_STATUS
+                    .getContent();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        }
+    }
+
+    public static void ensureTransactionIsStarted(final Connection connection) {
+
+        ensureConnectionIsOpen(connection);
+
+        try {
+            if (connection.getAutoCommit()) {
+                var userMessage = MessagesEnum.USER_ERROR_TRANSACTION_IS_NOT_STARTED.getContent();
+                var technicalMessage = MessagesEnum.TECHNICAL_ERROR_TRANSACTION_IS_NOT_STARTED.getContent();
+                throw AuroraException.create(userMessage, technicalMessage);
+            }
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_TRANSACTION_IS_STARTED
+                    .getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_SQL_EXCEPTION_VALIDATING_TRANSACTION_IS_STARTED
+                    .getContent();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        } catch (final Exception exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_TRANSACTION_IS_STARTED
+                    .getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_TRANSACTION_IS_STARTED
+                    .getContent();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        }
+    }
+
+    public static void ensureTransactionIsNotStarted(final Connection connection) {
+
+        ensureConnectionIsOpen(connection);
+
+        try {
+            if (!connection.getAutoCommit()) {
+                var userMessage = MessagesEnum.USER_ERROR_TRANSACTION_IS_NOT_STARTED.getContent();
+                var technicalMessage = MessagesEnum.TECHNICAL_ERROR_TRANSACTION_IS_NOT_STARTED.getContent();
+                throw AuroraException.create(userMessage, technicalMessage);
+            }
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_TRANSACTION_IS_STARTED
+                    .getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_SQL_EXCEPTION_VALIDATING_TRANSACTION_IS_STARTED
+                    .getContent();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        } catch (final Exception exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_TRANSACTION_IS_STARTED
+                    .getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_CONNECTION_UNEXPECTED_ERROR_VALIDATING_TRANSACTION_IS_STARTED
+                    .getContent();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        }
+    }
+}
