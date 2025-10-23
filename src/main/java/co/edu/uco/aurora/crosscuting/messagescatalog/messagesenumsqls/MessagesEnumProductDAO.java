5c0bdb74f936729d0458566652646568dacf1bfb
Juan Pablo <juanpablo10056@gmail.com>
2025-10-23 11:14:09 -0500

    Se implemento la logica en ProductDAO

---
+++ src/main/java/co/edu/uco/aurora/crosscuting/messagescatalog/messagesenumsqls/MessagesEnumProductDAO.java
@@ -0,0 +1,118 @@
+package co.edu.uco.aurora.crosscuting.messagescatalog.messagesenumsqls;
+
+import co.edu.uco.aurora.crosscuting.helper.TextHelper;
+
+public enum MessagesEnumProductDAO {
+
+    // Errores para la operación de Inserción (INSERT) de Producto (PRODUCT)
+
+    USER_ERROR_SQL_INSERT_PRODUCT(
+            "Error al registrar la información del producto",
+            "Se ha presentado un problema tratando de registrar la información del producto. " +
+                    "Por favor intente de nuevo y si el problema persiste, contacte al administrador del sistema."),
+
+    TECHNICAL_ERROR_SQL_INSERT_PRODUCT(
+            "Error técnico al registrar la información del producto",
+            "Se ha presentado un problema al tratar de ejecutar el proceso de creación de un producto. " +
+                    "Por favor valide que la base de datos esté funcionando correctamente."),
+
+    USER_ERROR_SQL_UNEXPECTED_ERROR_INSERT_PRODUCT(
+            "Error inesperado al registrar la información del producto",
+            "Se ha presentado un problema inesperado tratando de registrar la información del producto. " +
+                    "Por favor intente nuevamente y si el problema persiste, contacte al administrador del sistema."),
+
+    TECHNICAL_ERROR_SQL_UNEXPECTED_ERROR_INSERT_PRODUCT(
+            "Error técnico inesperado al registrar la información del producto",
+            "Se ha presentado un problema técnico inesperado al tratar de ejecutar el proceso de creación del producto. " +
+                    "Revise los registros."),
+
+
+    // Errores para la operación de Actualización (UPDATE) de Producto (PRODUCT)
+
+    USER_ERROR_SQL_UPDATE_PRODUCT(
+            "Error al actualizar la información del producto",
+            "No fue posible actualizar los datos del producto. Por favor, verifique los datos ingresados e intente de nuevo."),
+
+    TECHNICAL_ERROR_SQL_UPDATE_PRODUCT(
+            "Error técnico al actualizar la información del producto",
+            "Se ha presentado un error de tipo SQLException al intentar actualizar el registro del producto en la base de datos."),
+
+    USER_ERROR_SQL_UNEXPECTED_ERROR_UPDATE_PRODUCT(
+            "Error inesperado al actualizar la información del producto",
+            "Ha ocurrido un error inesperado al intentar actualizar el registro del producto. Por favor, intente de nuevo más tarde."),
+
+    TECHNICAL_ERROR_SQL_UNEXPECTED_ERROR_UPDATE_PRODUCT(
+            "Error técnico inesperado al actualizar la información del producto",
+            "Se ha presentado un error inesperado (no-SQL) al actualizar el registro del producto. Revise la traza completa de la excepción."),
+
+
+    // Errores para la operación de Consulta por Filtro (FIND BY FILTER) de Producto (PRODUCT)
+
+    USER_ERROR_SQL_EXECUTING_FIND_BY_FILTER_PRODUCT(
+            "Error consultando información del producto por filtro",
+            "Se ha presentado un problema tratando de buscar información del producto por filtro en la base de datos. " +
+                    "Por favor, intente de nuevo y si el problema persiste, contacte al administrador del sistema."),
+
+    TECHNICAL_ERROR_SQL_EXECUTING_FIND_BY_FILTER_PRODUCT(
+            "Error técnico ejecutando consulta de producto por filtro",
+            "Se ha presentado un problema al tratar de ejecutar la sentencia de consulta por filtro para el producto. " +
+                    "Verifique que la sintaxis SQL de la consulta sea correcta."),
+
+    USER_ERROR_SQL_UNEXPECTED_ERROR_FIND_BY_FILTER_PRODUCT(
+            "Error inesperado al consultar información del producto por filtro",
+            "Ha ocurrido un problema inesperado al intentar buscar información del producto por filtro. " +
+                    "Por favor, intente nuevamente y si el problema persiste, contacte al administrador del sistema."),
+
+    TECHNICAL_ERROR_SQL_UNEXPECTED_ERROR_FIND_BY_FILTER_PRODUCT(
+            "Error técnico inesperado en la consulta de producto por filtro",
+            "Se ha presentado un error técnico inesperado (no-SQL) al intentar ejecutar la consulta por filtro de producto. " +
+                    "Revise la traza completa de la excepción."),
+
+
+    // Errores para la Operación de Mapeo (MAPPING) de Producto (PRODUCT)
+
+    USER_ERROR_SQL_MAPPING_PRODUCT(
+            "Error interpretando la información del producto",
+            "Se ha presentado un problema tratando de interpretar la información de un producto consultada desde la base de datos. " +
+                    "Por favor, contacte al administrador del sistema."),
+
+    TECHNICAL_ERROR_SQL_MAPPING_PRODUCT(
+            "Error técnico mapeando el ResultSet a ProductEntity",
+            "Se ha presentado un problema técnico al mapear un registro del ResultSet a la entidad ProductEntity. " +
+                    "Verifique que los nombres de las columnas coincidan."),
+
+    USER_ERROR_SQL_UNEXPECTED_ERROR_MAPPING_PRODUCT(
+            "Error inesperado mapeando la información del producto",
+            "Se ha presentado un problema inesperado tratando de interpretar la información del producto." +
+                    " Por favor, contacte al administrador del sistema."),
+
+    TECHNICAL_ERROR_SQL_UNEXPECTED_ERROR_MAPPING_PRODUCT(
+            "Error técnico inesperado en el mapeo de ProductEntity",
+            "Se ha presentado un error inesperado (no-SQL) durante el proceso de mapeo del ResultSet a la entidad ProductEntity. " +
+                    "Revise la traza completa de la excepción.");
+
+
+    private String title;
+    private String content;
+
+    private MessagesEnumProductDAO (final String title, final String content) {
+        setTitle(title);
+        setContent(content);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = TextHelper.getDefaultWithTrim(title);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = TextHelper.getDefaultWithTrim(content);
+    }
+}
--- src/main/java/co/edu/uco/aurora/data/dao/entity/sqls/ProductPostgresqlDAO.java
+++ src/main/java/co/edu/uco/aurora/data/dao/entity/sqls/ProductPostgresqlDAO.java
@@ -1,9 +1,19 @@
 package co.edu.uco.aurora.data.dao.entity.sqls;
 
+import co.edu.uco.aurora.crosscuting.exception.AuroraException;
+import co.edu.uco.aurora.crosscuting.helper.ObjectHelper;
+import co.edu.uco.aurora.crosscuting.helper.SqlConnectionHelper;
+import co.edu.uco.aurora.crosscuting.helper.TextHelper;
+import co.edu.uco.aurora.crosscuting.helper.UUIDHelper;
+import co.edu.uco.aurora.crosscuting.messagescatalog.messagesenumsqls.MessagesEnumProductDAO;
 import co.edu.uco.aurora.data.dao.entity.ProductDAO;
+import co.edu.uco.aurora.entity.CategoryEntity;
 import co.edu.uco.aurora.entity.ProductEntity;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public final class ProductPostgresqlDAO extends SqlConnection implements ProductDAO {
@@ -13,22 +23,184 @@ public final class ProductPostgresqlDAO extends SqlConnection implements Product
     }
 
     @Override
-    public void create(ProductEntity entity) {
+    public void create(final ProductEntity entity) {
+        SqlConnectionHelper.ensureTransactionIsStarted(getConnection());
 
+        final var sql = new StringBuilder();
+        sql.append("INSERT INTO Producto (id, nombre, idCategoria) ");
+        sql.append("VALUES (?, ?, ?)");
+
+        try (var preparedStatement = this.getConnection().prepareStatement(sql.toString())) {
+
+            preparedStatement.setObject(1, entity.getId());
+            preparedStatement.setString(2, entity.getName());
+            preparedStatement.setObject(3, entity.getCategory().getId()); // Asume que ProductEntity tiene getCategory()
+
+            preparedStatement.executeUpdate();
+
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnumProductDAO.USER_ERROR_SQL_INSERT_PRODUCT.getContent();
+            var technicalMessage = MessagesEnumProductDAO.TECHNICAL_ERROR_SQL_INSERT_PRODUCT.getContent()
+                    + ": " + exception.getMessage();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+
+        } catch (final Exception exception) {
+            var userMessage = MessagesEnumProductDAO.USER_ERROR_SQL_UNEXPECTED_ERROR_INSERT_PRODUCT.getContent();
+            var technicalMessage = MessagesEnumProductDAO.TECHNICAL_ERROR_SQL_UNEXPECTED_ERROR_INSERT_PRODUCT.getContent()
+                    + ": " + exception.getMessage();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        }
+    }
+
+    @Override
+    public void update(final ProductEntity entity) {
+        SqlConnectionHelper.ensureTransactionIsStarted(getConnection());
+
+        final var sql = "UPDATE Producto SET nombre = ?, idCategoria = ? WHERE id = ?";
+
+        try (var preparedStatement = this.getConnection().prepareStatement(sql)) {
+
+            preparedStatement.setString(1, entity.getName());
+            preparedStatement.setObject(2, entity.getCategory().getId());
+            preparedStatement.setObject(3, entity.getId());
+
+            preparedStatement.executeUpdate();
+
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnumProductDAO.USER_ERROR_SQL_UPDATE_PRODUCT.getContent();
+            var technicalMessage = MessagesEnumProductDAO.TECHNICAL_ERROR_SQL_UPDATE_PRODUCT.getContent()
+                    + ": " + exception.getMessage();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+
+        } catch (final Exception exception) {
+            var userMessage = MessagesEnumProductDAO.USER_ERROR_SQL_UNEXPECTED_ERROR_UPDATE_PRODUCT.getContent();
+            var technicalMessage = MessagesEnumProductDAO.TECHNICAL_ERROR_SQL_UNEXPECTED_ERROR_UPDATE_PRODUCT.getContent()
+                    + ": " + exception.getMessage();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        }
     }
 
     @Override
     public List<ProductEntity> findAll() {
-        return List.of();
+        return findByFilter(new ProductEntity());
     }
 
     @Override
-    public List<ProductEntity> findByFilter(ProductEntity filterEntity) {
-        return List.of();
+    public List<ProductEntity> findByFilter(final ProductEntity filterEntity) {
+        SqlConnectionHelper.ensureConnectionIsNotNull(getConnection());
+
+        var parameterList = new ArrayList<Object>();
+        var sql = createSentenceFindByFilter(filterEntity, parameterList);
+
+        try (var preparedStatement = this.getConnection().prepareStatement(sql)) {
+
+            for (int i = 0; i < parameterList.size(); i++) {
+                preparedStatement.setObject(i + 1, parameterList.get(i));
+            }
+
+            return executeSentenceFindByFilter(preparedStatement);
+        } catch (final AuroraException exception) {
+            throw exception;
+
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnumProductDAO.USER_ERROR_SQL_EXECUTING_FIND_BY_FILTER_PRODUCT.getContent();
+            var technicalMessage = MessagesEnumProductDAO.TECHNICAL_ERROR_SQL_EXECUTING_FIND_BY_FILTER_PRODUCT.getContent()
+                    + ": " + exception.getMessage();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+
+        } catch (final Exception exception) {
+            var userMessage = MessagesEnumProductDAO.USER_ERROR_SQL_UNEXPECTED_ERROR_FIND_BY_FILTER_PRODUCT.getContent();
+            var technicalMessage = MessagesEnumProductDAO.TECHNICAL_ERROR_SQL_UNEXPECTED_ERROR_FIND_BY_FILTER_PRODUCT.getContent()
+                    + ": " + exception.getMessage();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        }
     }
 
-    @Override
-    public void update(ProductEntity entity) {
+    private String createSentenceFindByFilter (final ProductEntity filterEntity, final List<Object> parameterList) {
+        final var sql = new StringBuilder();
 
+        sql.append("SELECT P.id, P.nombre, ");
+        sql.append("C.id AS idCategoria, C.nombre AS nombreCategoria ");
+        sql.append("FROM Producto P ");
+        sql.append("INNER JOIN Categoria C ON P.idCategoria = C.id ");
+
+        createWhereClauseFindByFilter(sql, parameterList, filterEntity);
+
+        sql.append(" ORDER BY P.nombre ASC");
+
+        return sql.toString();
+    }
+
+    private void createWhereClauseFindByFilter(final StringBuilder sql, final List<Object> parameterList,
+                                               final ProductEntity filterEntity) {
+
+        var filterEntityValidated = ObjectHelper.getDefault(filterEntity, new ProductEntity());
+        final var conditions = new ArrayList<String>();
+
+        addCondition(conditions, parameterList,
+                !UUIDHelper.getUUIDHelper().isDefaultUUID(filterEntityValidated.getId()),
+                "P.id = ?", filterEntityValidated.getId());
+
+        addCondition(conditions, parameterList,
+                !TextHelper.isEmptyWithTrim(filterEntityValidated.getName()),
+                "P.nombre = ?", filterEntityValidated.getName());
+
+        var categoryFilter = ObjectHelper.getDefault(
+                filterEntityValidated.getCategory(),
+                new CategoryEntity()
+        );
+
+        addCondition(conditions, parameterList,
+                !UUIDHelper.getUUIDHelper().isDefaultUUID(categoryFilter.getId()),
+                "C.id = ?", categoryFilter.getId());
+
+
+        if (!conditions.isEmpty()){
+            sql.append(" WHERE ");
+            sql.append(String.join(" AND ", conditions));
+        }
+    }
+
+    private void addCondition(final List<String> conditions, final List<Object> parameterList, final boolean codition,
+                              final String clause, final Object value) {
+        if (codition) {
+            conditions.add(clause);
+            parameterList.add(value);
+        }
+    }
+
+    private List<ProductEntity> executeSentenceFindByFilter(final PreparedStatement preparedStatement) {
+        var listProducts = new ArrayList<ProductEntity>();
+
+        try (var resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
+
+                var category = new CategoryEntity();
+                category.setId(UUIDHelper.getUUIDHelper().getFromString(resultSet.getString("idCategoria")));
+                category.setName(resultSet.getString("nombreCategoria"));
+
+                var product = new ProductEntity();
+                product.setId(UUIDHelper.getUUIDHelper().getFromString(resultSet.getString("id")));
+                product.setName(resultSet.getString("nombre"));
+
+                product.setCategory(category);
+
+                listProducts.add(product);
+            }
+
+        } catch (SQLException exception) {
+            var userMessage = MessagesEnumProductDAO.USER_ERROR_SQL_MAPPING_PRODUCT.getContent();
+            var technicalMessage = MessagesEnumProductDAO.TECHNICAL_ERROR_SQL_MAPPING_PRODUCT.getContent()
+                    + ": " + exception.getMessage();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        } catch (Exception exception) {
+            var userMessage = MessagesEnumProductDAO.USER_ERROR_SQL_UNEXPECTED_ERROR_MAPPING_PRODUCT.getContent();
+            var technicalMessage = MessagesEnumProductDAO.TECHNICAL_ERROR_SQL_UNEXPECTED_ERROR_MAPPING_PRODUCT.getContent()
+                    + ": " + exception.getMessage();
+            throw AuroraException.create(exception, userMessage, technicalMessage);
+        }
+
+        return listProducts;
     }
 }
